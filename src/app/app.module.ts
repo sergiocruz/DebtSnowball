9a3a824c7d83a076d3c86c165cbceb3a5ba9a516
Sergio Cruz <hello@sergiocruz.me>
2017-10-04 21:04:19 -0400

    Basic calculation done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { DebtService } from './debt.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [DebtService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/debt.ts
@@ -0,0 +1,4 @@
+export interface Debt {
+  name: string,
+  amount: number,
+}
